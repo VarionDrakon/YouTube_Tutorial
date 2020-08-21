6494bdf0207cdbc21c5145704cec375c480603ba
VarionDrakon <52794446+VarionDrakon@users.noreply.github.com>
2020-08-21 18:09:09 +0300

    Create String.cs

---
+++ C#/Variables/String.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Variable_type
+{
+    class main
+    {//Variable otcher
+        static void Main(string[] args)
+        {
+            string _string = "Hello Dragon \n Привет Дракон";
+
+            Console.WriteLine(_string);
+
+            Console.ReadKey(); 
+        }
+    }
+}
