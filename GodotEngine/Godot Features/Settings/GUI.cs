38c8330c890071298cdc4beaefda1a9bc512eee0
VarionDrakon <52794446+VarionDrakon@users.noreply.github.com>
2020-09-08 20:42:58 +0300

    GUI/UI in Godot!

---
+++ GodotEngine/Godot Features/Settings/GUI.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class GUI : Label
+{
+
+	public override void _Ready()
+	{
+		GetNode("Button_Menu").Connect("pressed", this, nameof(_on_Button_Menu_pressed));
+			
+	}
+
+	private void _on_Button_Menu_pressed()
+	{
+		GetTree().ChangeScene("res://Scenes_Scripts/Main_menu.tscn");
+	}
+}
