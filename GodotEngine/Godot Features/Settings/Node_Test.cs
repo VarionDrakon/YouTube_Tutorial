8e6c4cfe65f550b44353cead6ac76f9769a00597
VarionDrakon <52794446+VarionDrakon@users.noreply.github.com>
2020-09-28 20:03:40 +0300

    Animation in button

---
+++ GodotEngine/Godot Features/Settings/Node_Test.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class Node_Test : Node
+{
+	private void _on_Button_Show_pressed()
+	{
+		GetNode<Control>("Button_Show").Hide();
+		GetNode<Control>("Button_Hide").Show();
+
+		var Animated = GetNode<AnimationPlayer>("AnimationPlayer");
+		Animated.Play("Playing");
+	}
+
+
+	private void _on_Button_Hide_pressed()
+	{
+		GetNode<Control>("Button_Show").Show();
+		GetNode<Control>("Button_Hide").Hide();
+
+		var Animated = GetNode<AnimationPlayer>("AnimationPlayer");
+		Animated.Play("Backed");
+	}
+}
+
+
+
